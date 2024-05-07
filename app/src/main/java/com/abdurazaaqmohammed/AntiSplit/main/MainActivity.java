2b92971261c4382e513c50a4116bd5eb18325c1c
AbdurazaaqMohammed <56937889+AbdurazaaqMohammed@users.noreply.github.com>
2024-05-06 21:54:32 -0400

    Update MainActivity.java
    Add logging
    Run in background

--- app/src/main/java/com/abdurazaaqmohammed/AntiSplit/main/MainActivity.java
+++ app/src/main/java/com/abdurazaaqmohammed/AntiSplit/main/MainActivity.java
@@ -6,14 +6,18 @@ import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.OpenableColumns;
 import android.view.View;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.content.SharedPreferences;
 
 import com.abdurazaaqmohammed.AntiSplit.R;
+import com.reandroid.apkeditor.merge.LogUtil;
 import com.reandroid.apkeditor.merge.Merger;
 
 import java.io.File;
@@ -21,13 +25,14 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.util.zip.ZipEntry;
+import java.lang.ref.WeakReference;
 
-public class MainActivity extends Activity {
-    private final int REQUEST_CODE_OPEN_SPLIT_APK_TO_ANTISPLIT = 1;
-    private final int REQUEST_CODE_SAVE_APK = 2;
-
-    private final boolean isOldAndroid = Build.VERSION.SDK_INT<19;
+public class MainActivity extends Activity implements Merger.LogListener {
+    private final static int REQUEST_CODE_OPEN_SPLIT_APK_TO_ANTISPLIT = 1;
+    private final static int REQUEST_CODE_SAVE_APK = 2;
+    private final static boolean isOldAndroid = Build.VERSION.SDK_INT<19;
+    private static boolean logEnabled;
+    private static String newFileName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,21 +45,53 @@ public class MainActivity extends Activity {
         if (isOldAndroid) {
             findViewById(R.id.oldAndroidInfo).setVisibility(View.VISIBLE);
             // Android versions below 4.4 are too old to use the file picker for ACTION_OPEN_DOCUMENT/ACTION_CREATE_DOCUMENT. The location of the file must be manually input. The files will be saved to Download folder in the internal storage.
+        } else {
+            findViewById(R.id.workingFileField).setVisibility(View.INVISIBLE);
         }
+
+        SharedPreferences settings = getSharedPreferences("set", Context.MODE_PRIVATE);
+
+        // Fetch settings from SharedPreferences
+        logEnabled = settings.getBoolean("logEnabled", true);
+        LogUtil.setLogListener(this);
+        LogUtil.setLogEnabled(logEnabled);
+
+        Switch logSwitch = findViewById(R.id.logSwitch);
+        logSwitch.setChecked(logEnabled);
+        logSwitch.setOnCheckedChangeListener((buttonView, isChecked) ->{
+         logEnabled = isChecked;
+         LogUtil.setLogEnabled(logEnabled);
+        });
 
         findViewById(R.id.decodeButton).setOnClickListener(v -> openFilePickerOrStartProcessing());
         // Check if user shared or opened file with the app.
         final Intent fromShareOrView = getIntent();
         final String fromShareOrViewAction = fromShareOrView.getAction();
+        Uri sharedUri = null;
         if (Intent.ACTION_SEND.equals(fromShareOrViewAction)) {
-            Uri sharedUri = fromShareOrView.getParcelableExtra(Intent.EXTRA_STREAM);
-            if (sharedUri != null) process(sharedUri);
+            sharedUri = fromShareOrView.getParcelableExtra(Intent.EXTRA_STREAM);
         } else if (Intent.ACTION_VIEW.equals(fromShareOrViewAction)) {
-            Uri sharedUri = fromShareOrView.getData();
-            if (sharedUri != null) process(sharedUri);
+            sharedUri = fromShareOrView.getData();
+        }
+        if (sharedUri != null) {
+            new ProcessTask(this).execute(sharedUri);
         }
     }
+    @Override
+    protected void onPause() {
+        final SharedPreferences settings = getSharedPreferences("set", Context.MODE_PRIVATE);
+        settings.edit().putBoolean("logEnabled", logEnabled).apply();
+        super.onPause();
+    }
 
+    @Override
+    public void onLog(String log) {
+        runOnUiThread(() -> {
+            TextView logTextView = findViewById(R.id.logField);
+            logTextView.append(log + "\n");
+            logTextView.scrollTo(0, logTextView.getBottom());
+        });
+    }
     @SuppressLint("InlinedApi")
     private void openFilePickerOrStartProcessing() {
         if (isOldAndroid) {
@@ -75,9 +112,11 @@ public class MainActivity extends Activity {
     }
 
     private void process(Uri splitAPKUri) {
+        clearLogField();
+        newFileName = getOriginalFileName(this, splitAPKUri);
         try {
             Merger.run(getContentResolver().openInputStream(splitAPKUri), getExternalCacheDir());
-            openFileManagerToSaveAPK(splitAPKUri);
+            LogUtil.logMessage(getString(R.string.success));
         } catch (IOException e) {
             showError(e.toString());
         }
@@ -125,7 +164,7 @@ public class MainActivity extends Activity {
             File file = filesList[i];
             if(file.isDirectory()){
                 deleteEmptyDirectories(file);
-            }
+}
         }
         filesList = dir.listFiles();
         if(filesList == null || filesList.length == 0){
@@ -140,48 +179,91 @@ public class MainActivity extends Activity {
         super.onDestroy();
     }
 
-    public static File newFile(File outputDir, ZipEntry zipEntry) throws IOException {
-        File file = new File(outputDir, zipEntry.getName());
-        String canonicalizedPath = file.getCanonicalPath();
-        if (!canonicalizedPath.startsWith(outputDir.getCanonicalPath() + File.separator)) {
-            throw new IOException("Zip entry is outside of the target dir: " + zipEntry.getName());
+
+    private static class ProcessTask extends AsyncTask<Uri, Void, String> {
+        private WeakReference<MainActivity> activityReference;
+
+        // only retain a weak reference to the activity
+            ProcessTask(MainActivity context) {
+            activityReference = new WeakReference<>(context);
         }
-        return file;
+        @Override
+        protected String doInBackground(Uri... uris) {
+            MainActivity activity = activityReference.get();
+
+            deleteDir(activity.getExternalCacheDir());
+            activity.process(uris[0]);
+            return activity.getString(R.string.success);
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            MainActivity activity = activityReference.get();
+            Toast.makeText(activity, result, Toast.LENGTH_SHORT).show();
+
+            activity.openFileManagerToSaveAPK();
+        }
     }
+
+    private static class CopyTask extends AsyncTask<Uri, Void, String> {
+        private WeakReference<MainActivity> activityReference;
+
+        // only retain a weak reference to the activity
+        CopyTask(MainActivity context) {
+            activityReference = new WeakReference<>(context);
+        }
+        @Override
+        protected String doInBackground(Uri...uris) {
+            MainActivity activity = activityReference.get();
+
+            // This is stupid figure out how to refactor it to write to the output URI from ACTION_CREATE_DOCUMENT directly
+            File fileInCacheDir = new File(activity.getExternalCacheDir() + File.separator + "AntiSplit M Output.apk");
+            InputStream inputStream = null;
+            try {
+                inputStream = new FileInputStream(fileInCacheDir);
+
+            OutputStream outputStream = activity.getContentResolver().openOutputStream(uris[0]);
+
+            byte[] buffer = new byte[1024];
+            int length;
+            while ((length = inputStream.read(buffer)) > 0) {
+                outputStream.write(buffer, 0, length);
+            }
+
+            inputStream.close();
+            outputStream.close();
+            } catch (IOException e) {
+                activity.showError(e.toString());
+            }
+            return activity.getString(R.string.success_saved);
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            MainActivity activity = activityReference.get();
+            Toast.makeText(activity.getApplicationContext(), result, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void clearLogField() {
+        TextView logTextView = findViewById(R.id.logField);
+        logTextView.setText("");
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (resultCode == RESULT_OK && data != null) {
             Uri uri = data.getData();
             if (uri != null) {
-                try {
-                   switch(requestCode) {
-                       case REQUEST_CODE_OPEN_SPLIT_APK_TO_ANTISPLIT:
-                           deleteDir(getExternalCacheDir());
-                           process(uri);
-                           break;
-                       case REQUEST_CODE_SAVE_APK:
-                           // This is stupid figure out how to refactor it to write to the output URI from ACTION_CREATE_DOCUMENT directly
-                           File fileInCacheDir = new File(getExternalCacheDir() + File.separator + "AntiSplit M Output.apk");
-                           InputStream inputStream = new FileInputStream(fileInCacheDir);
-                           OutputStream outputStream = getContentResolver().openOutputStream(uri);
-
-                           byte[] buffer = new byte[1024];
-                           int length;
-                           while ((length = inputStream.read(buffer)) > 0) {
-                               outputStream.write(buffer, 0, length);
-                           }
-
-                           inputStream.close();
-                           outputStream.close();
-                           Toast.makeText(this, "File saved successfully", Toast.LENGTH_SHORT).show();
-                           TextView errorBox = findViewById(R.id.errorField);
-                           errorBox.setVisibility(View.INVISIBLE);
+               switch(requestCode) {
+                   case REQUEST_CODE_OPEN_SPLIT_APK_TO_ANTISPLIT:
+                       new ProcessTask(this).execute(uri);
                        break;
-                   }
-                } catch (IOException e) {
-                    showError(e.toString());
-                }
+                   case REQUEST_CODE_SAVE_APK:
+                       new CopyTask(this).execute(uri);
+                   break;
+               }
             }
         }
     }
@@ -208,14 +290,14 @@ public class MainActivity extends Activity {
                 result = result.substring(cut + 1);
             }
         }
-        return result;
+        return result.replaceFirst("\\.(apks|xapk|apkm)", "_antisplit");
     }
 
-    private void openFileManagerToSaveAPK(Uri splitAPKUri) {
+    private void openFileManagerToSaveAPK() {
         @SuppressLint("InlinedApi") Intent intent = new Intent(Intent.ACTION_CREATE_DOCUMENT);
         intent.addCategory(Intent.CATEGORY_OPENABLE);
         intent.setType("application/vnd.android.package-archive");
-        intent.putExtra(Intent.EXTRA_TITLE, getOriginalFileName(this, splitAPKUri).replace(".apks", "_antisplit").replace(".xapk", "_antisplit").replace(".apkm", "_antisplit"));
+        intent.putExtra(Intent.EXTRA_TITLE, newFileName);
         startActivityForResult(intent, REQUEST_CODE_SAVE_APK);
     }
 }
